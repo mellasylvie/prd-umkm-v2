44181f41129b861bbffcaaedea5eee36abcdc959
MELLANIA PERMATA SYLVIE <mellaniapermata@gmail.com>
2025-11-29 07:15:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5617081910-7f1d6",
+  "appId": "1:105934825526:web:edea6d609f322b4be1d097",
+  "apiKey": "AIzaSyASrb2-uqpf-5I8U3eGjNYS5gxO3IGYpgA",
+  "authDomain": "studio-5617081910-7f1d6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "105934825526"
+};
